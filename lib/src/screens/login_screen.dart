05044443ec60110fb18f2fe16ca7ff8789c4fb38
Kelven Galvão <kelvengalvao@pop-os.localdomain>
2019-04-27 17:20:51 -0300

    Start code of BLoC, add validators and create login screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -7,9 +7,39 @@ class LoginScreen extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Column(
         children: <Widget>[
-          
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 20)),
+          submitButton(),
         ],
       ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'yourname@example.com',
+        labelText: 'Email',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
     );
   }
 }
