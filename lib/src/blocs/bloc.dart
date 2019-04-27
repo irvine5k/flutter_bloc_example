ed4172fbbc47e462a348d47b92190a1223b2184b
Kelven Galvão <kelvengalvao@pop-os.localdomain>
2019-04-27 17:23:21 -0300

    Add dispose on streams

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -2,14 +2,19 @@ import 'dart:async';
 import 'validators.dart';
 
 class Bloc with Validators {
-  final _emailController = StreamController<String>();
-  final _passwordController = StreamController<String>();
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
 
   // Add data to stream
-  Stream<String> get email => _emailController.stream.transform(validateEmail);
-  Stream<String> get password => _passwordController.stream.transform(validatePassword);
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
 
   // Change data
-  Function(String) get changeEmail => _emailController.sink.add;
-  Function(String) get changePassword => _passwordController.sink.add;
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
